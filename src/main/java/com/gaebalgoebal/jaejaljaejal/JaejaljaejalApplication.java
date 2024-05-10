e5b77e2fa8f11df64624bc235b844fc1931f7d30
moonju lee <mjlee4327@gmail.com>
2024-05-10 19:11:49 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/gaebalgoebal/jaejaljaejal/JaejaljaejalApplication.java
@@ -0,0 +1,13 @@
+package com.gaebalgoebal.jaejaljaejal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaejaljaejalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaejaljaejalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaebalgoebal/jaejaljaejal/JaejaljaejalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaebalgoebal.jaejaljaejal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaejaljaejalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
